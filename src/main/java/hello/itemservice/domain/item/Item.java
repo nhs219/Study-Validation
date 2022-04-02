b417a9a2716175da5d5e90cba5f6c9e62f787d01
nhs219 <heesun_z@naver.com>
2022-04-02 21:13:46 +0900

    validation

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -9,13 +9,4 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
-
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }
